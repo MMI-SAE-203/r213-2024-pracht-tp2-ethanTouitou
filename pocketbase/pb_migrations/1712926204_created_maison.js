bc1a45f5f91d1e46ad6d274c9022264b590bf06b
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2024-05-24 13:49:22 +0200

    sxdrftcgvyh

---
+++ pocketbase/pb_migrations/1712926204_created_maison.js
@@ -0,0 +1,137 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const collection = new Collection({
+    "id": "pclpx6rjr961xzc",
+    "created": "2024-04-12 12:50:04.474Z",
+    "updated": "2024-04-12 12:50:04.474Z",
+    "name": "maison",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "7wiqoyov",
+        "name": "nomMaison",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "jkwlzhxo",
+        "name": "imageMaison",
+        "type": "file",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "mimeTypes": [],
+          "thumbs": [],
+          "maxSelect": 1,
+          "maxSize": 5242880,
+          "protected": false
+        }
+      },
+      {
+        "system": false,
+        "id": "kb7cr7ve",
+        "name": "prixMaison",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "rriazts1",
+        "name": "nombreDeChambre",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "vhg10peg",
+        "name": "nombreSalleDeBain",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "bgixcgld",
+        "name": "adresse",
+        "type": "text",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "nrcqdozg",
+        "name": "surface",
+        "type": "number",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "noDecimal": false
+        }
+      },
+      {
+        "system": false,
+        "id": "kt7mbxhv",
+        "name": "favori",
+        "type": "bool",
+        "required": false,
+        "presentable": false,
+        "unique": false,
+        "options": {}
+      }
+    ],
+    "indexes": [],
+    "listRule": null,
+    "viewRule": null,
+    "createRule": null,
+    "updateRule": null,
+    "deleteRule": null,
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("pclpx6rjr961xzc");
+
+  return dao.deleteCollection(collection);
+})
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,18 @@
 <script setup lang="ts">
-import type { MaisonRecord } from '@/types'
+import { pb } from '@/backend';
 import MaisonCard from '@/components/MaisonCard.vue'
-const maisonsListe: MaisonRecord[] = [
-  {
-    adresse: '2821 Lake Sevilla, Palm Harbor, TX',
-    collectionId: 'sidpkt9hmbfwoeb',
-    collectionName: 'maison',
-    created: '2023-05-01 14:00:54.598Z',
-    favori: false,
-    id: '64l3n8nar836i5j',
-    image: 'pexels_binyamin_mellish_106399_bnHdy6H03e.jpg',
-    nbChambres: 4,
-    nbSdb: 2,
-    nomMaison: 'Beverly Springfield',
-    prix: 2700,
-    surface: 45,
-    updated: '2024-04-02 13:49:11.230Z'
-  },
-  {
-    adresse: '2699 Green Valley, Highland Lake',
-    collectionId: 'sidpkt9hmbfwoeb',
-    collectionName: 'maison',
-    created: '2024-04-02 13:48:47.643Z',
-    favori: true,
-    id: 'lo831zeg10hpm9y',
-    image: 'pexels_binyamin_mellish_106399_YpoLMcQJsi.png',
-    nbChambres: 4,
-    nbSdb: 2,
-    nomMaison: 'Palm Harbor',
-    prix: 2095,
-    surface: 42,
-    updated: '2024-04-02 13:48:47.643Z'
-  },
-  {
-    adresse: '909 Woodland St, Michigan, IN',
-    collectionId: 'sidpkt9hmbfwoeb',
-    collectionName: 'maison',
-    created: '2024-04-02 13:52:17.823Z',
-    favori: false,
-    id: 's1zuhj19x8eb242',
-    image: 'faulkner_ave_ZHz3nhmJzC.png',
-    nbChambres: 4,
-    nbSdb: 3,
-    nomMaison: 'Faulkner Ave',
-    prix: 4550,
-    surface: 80,
-    updated: '2024-04-02 13:52:17.823Z'
-  }
-]
+const maisonsListe = await pb.collection('maison').getFullList({
+  filter: 'favori=true',
+});
+console.log(maisonsListe);
+
 //
 </script>
 
 <template>
   <h1 class="text-2xl">Bonjour monde !</h1>
+  <!--  <MaisonCard v-bind="maisonsListe[0]" />
+  <MaisonCard v-bind="maisonsListe[1]" /> -->
+
+  <MaisonCard v-for="uneMaison in maisonsListe" :key="uneMaison.id" v-bind="uneMaison" />
 </template>
